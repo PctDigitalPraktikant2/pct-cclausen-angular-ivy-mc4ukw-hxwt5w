2764b78eeae029442d8263661cad87fad8c53c1a
PctDigitalPraktikant2 <pctdigitalpraktikant2@gmail.com>
2022-08-29 14:38:31 +0200

    4 files modified

--- src/app/menu/scores/scores-display/scores-display.component.ts
+++ src/app/menu/scores/scores-display/scores-display.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 import { crown } from '../../../images';
+import { BackendService, HighScoreEntry } from '../../../backend.service';
+
 @Component({
   selector: 'app-scores-display',
   templateUrl: './scores-display.component.html',
   styleUrls: ['./scores-display.component.css']
 })
-export class ScoresDisplayComponent implements OnInit {
 
-  constructor() { }
+export class ScoresDisplayComponent implements OnInit {
+public scores: HighScoreEntry[]=[]
+  constructor(private backend:BackendService) { }
   crown=crown
-  ngOnInit() {
+
+  async ngOnInit() {
+    this.scores= await this.backend.getHighscores()
   }
 
 }
